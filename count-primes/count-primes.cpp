be24e5c2a6b4534c8f5887baabdd74a228a21dd8
SRISHTI RAJ <rajshrishti20@gmail.com>
2022-04-24 12:57:39 +0530

    Time: 435 ms, Memory: 130.6 MB - LeetHub

--- count-primes/count-primes.cpp
+++ count-primes/count-primes.cpp
@@ -5,7 +5,7 @@ public:
         if(n==0||n==1) return 0;
         p[1]=p[0]=0;
         for(int i=2;i*i<=n;i++){
-            if(p[i]==1){
+            if(p[i]){
                 for(int j=2*i;j<n;j=j+i){
                     p[j]=0;
                 }
