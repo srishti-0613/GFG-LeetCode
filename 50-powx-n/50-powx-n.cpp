67c3b5033bc2ace26d8cae49866bc0e439a9ca92
SRISHTI RAJ <rajshrishti20@gmail.com>
2022-04-28 22:01:49 +0530

    Time: 4 ms (26.55%), Space: 6.1 MB (13.61%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double myPow(double x, int n) {
       double cal=pow(x,n)  ;
-        cout<< fixed<<setprecision(5)<<cal<<endl;
+      
         return cal;
     }
 };
