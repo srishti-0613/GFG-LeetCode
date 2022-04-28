4eb66f48e13abb57a999664a65290c5ea1394a28
SRISHTI RAJ <rajshrishti20@gmail.com>
2022-04-28 22:49:05 +0530

    Time: 27 ms (11.05%), Space: 6 MB (26.78%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int mySqrt(int x) {
-      int n;
-        for(long long i=1;i*i>n;i++){
+     
+      for(long long i=1;i<=x;i++){
             if(i*i==x) return i;
             if(i*i>x) return i-1;
         }
-        return n;
+        return 0;
     }
 };
